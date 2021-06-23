9ba79cba54a30c6f67657917160ab5038a4abe97
Picsong <312465087@qq.com>
2021-06-23 10:12:03 +0800

    脚手架完善

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <div>
     <img class="h-7" alt="Vue logo" src="./assets/logo.png" />
-    <a-button @click="info">123</a-button>
+    <a-button @click="info">
+      {{ text }}
+    </a-button>
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
+  import { defineComponent, ref } from 'vue'
   import { message } from 'ant-design-vue'
   export default defineComponent({
     name: 'App',
     setup() {
+      const text = ref(import.meta.env.VITE_APP_TITLE)
       const info = () => {
         message.info('This is a normal message')
       }
       return {
-        info
+        info,
+        text
       }
     }
   })
